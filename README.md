44c2bb595859b6d6d11c1cdb7ebd88c6840e2cc2
Krich2020 <keston@therichfamily.net>
2023-09-18 23:46:29 -0600

    Finished the high score feature. It now saves in the local storage and retreives the higscores in the local storage. I also updated the readme.md and fixed various stylings and html content.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Crunch Time
+
+Challenge your knowledge of JavaScript with this exciting timed quiz! Each question will test your JavaScript skills, and you'll have 20 seconds to answer. Why not invite your friends to join and see who's the JavaScript master among you?
+
+## Usage
+
+![a gif of how to use CrunchTime](/assets/images/crunchTime.gif)
+
+Visit [Crunch Time](https://krich2022.github.io/crunch-time/) and press 'Start Test' to begin the quiz. You have 20 seconds to finish the 10 questions on this quiz.The scores are from 0-10. Check the high scores to see who did the best!
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,64 +1,156 @@
-const score = {
-   correct: 0,
-   wrong: 0,
-};
+let score = 0;
+const correctQuestions = ["q1a", "q2d", "q3d", "q4a", "q5a", "q6b", "q7b", "q8b", "q9a", "q10c"];
 let questions = document.querySelectorAll('[id*="question"]');
 const start = document.getElementById("start-test");
 const startContainer = document.getElementById("start");
 let currentQuestion = 1;
 let previousQuestion = 0;
 const timer = document.getElementById("timer");
-let timeLeft = 200;
+let timeLeft = 0;
 const timerContainer = document.querySelector(".timer-container");
 const timesUp = document.getElementById("time-up");
 const allDone = document.getElementById("end");
 const nextQuestionButtons = document.querySelectorAll(".button-question");
+const retakeTest = document.getElementById("retake-test");
+const submitScore = document.getElementById("submit-score");
+const allScores = document.getElementById("high-score");
+let highScores = [];
+
+document.getElementById("view-high-scores").addEventListener("click", () => {
+    retrieveHighScore();
+    endTest();
+    allDone.classList.add("hidden");
+    allScores.classList.remove("hidden");
+    startContainer.classList.add("hidden");
+    timerContainer.classList.add("timer-hidden");
+    timeLeft = 0;
+});
+
+document.getElementById("start-over").addEventListener("click", restart);
+
+submitScore.addEventListener("click", updateHighScore);
+
+retakeTest.addEventListener("click", restart);
 
 start.addEventListener("click", startTest);
+
 nextQuestionButtons.forEach((button) => {
-   button.addEventListener("click", nextQuestion);
+    button.addEventListener("click", () => {
+        checkQuestion(button);
+        nextQuestion();
+    });
 });
 function startTest() {
-   startContainer.classList.add("hidden");
-   questions[0].classList.remove("hidden");
-   startTimer();
+    startContainer.classList.add("hidden");
+    questions[0].classList.remove("hidden");
+    timerContainer.classList.remove("timer-hidden");
+    timeLeft = 200;
+    startTimer();
 }
 
 function nextQuestion() {
-   if (currentQuestion < questions.length) {
-      questions[currentQuestion].classList.remove("hidden");
-      currentQuestion++;
-      console.log(currentQuestion);
-   }
-   if (currentQuestion != 0) {
-      hideQuestion();
-   }
+    if (currentQuestion < questions.length) {
+        questions[currentQuestion].classList.remove("hidden");
+
+        console.log(currentQuestion);
+    }
+    if (currentQuestion != 0) {
+        currentQuestion++;
+
+        hideQuestion();
+    }
 }
 
 function startTimer() {
-   timer.innerHTML = timeLeft;
-   if (timeLeft === 0) {
-      timerContainer.classList.add("hidden");
-      endTest();
-   } else {
-      timeLeft--;
-      setTimeout(startTimer, 1000);
-   }
+    timer.innerHTML = timeLeft;
+    if (!allScores.classList.contains("hidden")) {
+        return;
+    } else if (timeLeft === 0 || currentQuestion === 11) {
+        timerContainer.classList.add("timer-hidden");
+        endTest();
+    } else {
+        timeLeft--;
+        setTimeout(startTimer, 1000);
+    }
 }
 
 function endTest() {
-   for (let i = 0; i < questions.length; i++) {
-      questions[i].classList.add("hidden");
-   }
-   allDone.classList.remove("hidden");
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].classList.add("hidden");
+    }
+    allDone.classList.remove("hidden");
+    document.getElementById("user-score").innerHTML = score;
 }
 
 function hideQuestion() {
-   if (currentQuestion !== previousQuestion) {
-      questions[previousQuestion].classList.add("hidden");
-      previousQuestion++;
-   }
-   if (currentQuestion === 10) {
-      endTest();
-   }
+    if (currentQuestion !== previousQuestion) {
+        questions[previousQuestion].classList.add("hidden");
+        previousQuestion++;
+    }
+    if (currentQuestion === 11) {
+        endTest();
+    }
+}
+
+function checkQuestion(e) {
+    let isCorrect = false;
+    for (let i = 0; i < correctQuestions.length; i++) {
+        if (e.id === correctQuestions[i]) {
+            isCorrect = true;
+        }
+    }
+    if (isCorrect) {
+        score++;
+    }
+}
+
+function restart() {
+    currentQuestion = 1;
+    previousQuestion = 0;
+    score = 0;
+    allDone.classList.add("hidden");
+    allScores.classList.add("hidden");
+
+    startTest();
+}
+
+function updateHighScore() {
+    let name = document.getElementById("score").value.trim();
+    if (name === "") {
+        alert("Please enter your initials");
+    } else {
+        let newHighScore = { [name]: score };
+        retrieveHighScore(newHighScore);
+        allDone.classList.add("hidden");
+        allScores.classList.remove("hidden");
+    }
+}
+
+function retrieveHighScore(e) {
+    highScores = [];
+    let highScoreList = document.getElementById("scores");
+    let retrievedScores = localStorage.getItem("highScores");
+
+    if (!retrievedScores && !e) {
+        highScoreList.innerHTML = "No High Scores available";
+        return;
+    } else if (retrievedScores) {
+        highScores = JSON.parse(retrievedScores);
+    }
+
+    if (e) {
+        highScores.push(e);
+    }
+    highScores = highScores.filter((item) => item !== null);
+    highScores.sort((a, b) => b[Object.keys(b)[0]] - a[Object.keys(a)[0]]);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    highScoreList.innerHTML = "";
+    console.log(highScores);
+    for (let i = 0; i < highScores.length; i++) {
+        let element = document.createElement("li");
+
+        element.textContent = Object.keys(highScores[i])[0] + ": " + Object.values(highScores[i])[0];
+        highScoreList.appendChild(element);
+    }
 }
